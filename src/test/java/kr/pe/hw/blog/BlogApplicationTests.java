c06a681c50703d9f5844f007c677865c15f79f1d
Clarus23 <dnjs3023@gmail.com>
2024-08-13 16:15:58 +0900

    Project 생성

---
+++ src/test/java/kr/pe/hw/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.hw.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
