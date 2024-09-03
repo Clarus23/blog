8697ab861f4a0ccaa840d83affabc078fd23d3de
Clarus23 <dnjs3023@gmail.com>
2024-09-03 15:31:52 +0900

    업로드 시간이 입력되지 않는 문제 수정

--- src/main/java/kr/pe/hw/blog/domain/BaseTimeEntity.java
+++ src/main/java/kr/pe/hw/blog/domain/BaseTimeEntity.java
@@ -1,5 +1,10 @@
 package kr.pe.hw.blog.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -14,9 +19,15 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class) //BaseTimeEntity 클래스에 Auditing 기능을 포함시킴
 public abstract class BaseTimeEntity { //추상화 이유
     //JPA Auditing 기능을 이용한 생성일자, 수정일자 자동화
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @CreatedDate
     private LocalDateTime createdDate; //Entity가 생성되어 저장될 때 시간이 자동 저장
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @LastModifiedDate
     private LocalDateTime modifiedDate; //조회한 Entity의 값을 변경할 때 시간이 자동 저장
 }
